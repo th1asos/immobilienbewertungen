d704b0af4854000af5e5e6329a3e628637ba6741
th1asos <126890093+th1asos@users.noreply.github.com>
2023-06-04 12:13:43 +0200

    Add files via upload

---
+++ pages/09-Änderungen.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.set_page_config(page_title="Immobilienbewertung", page_icon="logo-thiasos.png")
+
+st.header("Letzte Änderungen")
+
+st.subheader("Version 0.1 vom 04. Juni 2023")
+st.text("Änderungen:")
+st.caption("* Download und Upload-Möglichkeit entfernt, da dieser bei Github nicht funktioniert.")
+st.caption("* Zinskosten, die durch den Zuschlag bis zum Verteilungstermin entstehen, im Tab 'Erweiterte Anschaffungskosten' hinzugefügt.")
+st.caption("* Diese Seite hinzugefügt.")
